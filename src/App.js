bf78e78ead1c740ef6c20cc6b21749f247c6b69c
Art <art.vanhandenhoven@gmail.com>
2022-04-30 15:57:02 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <React.Fragment>
-      <div></div>
+      <div>
+        <p>
+          Couter: <span>{count}</span>
+        </p>
+        <button onClick={handleIncre}>Incr</button>
+      </div>
     </React.Fragment>
   );
+
+  function handleIncre() {
+    setCount(count + 1);
+  }
 }
 
 export default App;
